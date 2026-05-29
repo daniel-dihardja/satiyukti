e3118e54f6b020dc115e7c70b7f25a1c004fe4bd
Daniel Dihardja <daniel@dihardja.de>
2026-05-29 20:14:24 +0200

    update styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,14 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Lato, Cormorant_Garamond } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+  variable: '--font-sans',
+})
 
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+const cormorant = Cormorant_Garamond({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-heading',
 })
 
 export default function RootLayout({
@@ -20,7 +25,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn("antialiased", lato.variable, cormorant.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
